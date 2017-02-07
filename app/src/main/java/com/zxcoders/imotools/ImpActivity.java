8e3c64140e554fe26571caa5a8e77d08d226d49e
Marco Martins <marcopaulomartins@hotmail.com>
2017-02-07 22:36:07 +0000

    Botoes grandes

--- app/src/main/java/com/zxcoders/imotools/ImpActivity.java
+++ app/src/main/java/com/zxcoders/imotools/ImpActivity.java
@@ -22,7 +22,12 @@ public class ImpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         setContentView(R.layout.activity_imp);
+
     }
 
     private void buttonEvent() {
--- app/src/main/java/com/zxcoders/imotools/ValueOfSaleActivity.java
+++ app/src/main/java/com/zxcoders/imotools/ValueOfSaleActivity.java
@@ -20,8 +20,12 @@ public class ValueOfSaleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_value_of_sale);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        setContentView(R.layout.activity_value_of_sale);
 
     }
 
